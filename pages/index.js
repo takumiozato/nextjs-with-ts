41fcbc6cb0c4703b6c3ec519043074c4479b1cc6
takumiozato <dolphin.rokujiro@gmail.com>
2022-08-17 22:54:20 +0900

    ちょっと更新

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>Next.jsを勉強中のフロントエンドエンジニアです。</p>
         <p>
           (This is a sample website - you’ll be building a site like this in{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
