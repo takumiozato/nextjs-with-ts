9bd74f9bddf108c90f5b541d15c9459ca74d9ecf
takumiozato <dolphin.rokujiro@gmail.com>
2022-08-24 19:29:49 +0900

    fix top page,layout

--- pages/imagelist/default/index.tsx
+++ pages/imagelist/default/index.tsx
@@ -1,23 +1,26 @@
 import styles from './index.module.css'
+import Layout from '../../../components/layout'
 
 const Page = () => {
   const dummyImages = [...Array(1000)]
   return (
-    <div className={styles.container}>
-      <p>normal image</p>
-      <div className={styles.image_container}>
-        {dummyImages.map((_, i) => {
-          return (
-            <img
-              key={`defaultImage-${i}`}
-              src={`https://picsum.photos/400/200?random=${i}`}
-              width={400}
-              height={200}
-            />
-          )
-        })}
+    <Layout>
+      <div className={styles.container}>
+        <p>normal image</p>
+        <div className={styles.image_container}>
+          {dummyImages.map((_, i) => {
+            return (
+              <img
+                key={`defaultImage-${i}`}
+                src={`https://picsum.photos/400/200?random=${i}`}
+                width={400}
+                height={200}
+              />
+            )
+          })}
+        </div>
       </div>
-    </div>
+    </Layout>
   )
 }
 
